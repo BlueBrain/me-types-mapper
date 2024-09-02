e484b94bd4108348c75fc60a1538dd03c8e03870
yann.roussel <yann.roussel@epfl.ch>
2024-09-02 17:22:26 +0200

    added Read the Docs

---
+++ conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'me-types-mapper'
+copyright = '2024, Yann Roussel'
+author = 'Yann Roussel'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Required
+version: 2
+
+sphinx:
+  configuration: ./conf.py
+  fail_on_warning: true
+
+python:
+  install:
+    - method: pip
+      path: .
+    - requirements: requirements_docs.txt
